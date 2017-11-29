6b69f4fb0c3850a8ebc0094c851ca00fa01ecb66
Mona Lisa <SwagwardDaniels@Edwards-MacBook-Pro-2.local>
2017-11-28 23:36:58 -0800

    initial

---
+++ foodArray.js
@@ -0,0 +1,11 @@
+module.exports = [
+"squash",
+"beans",
+"turkey",
+"stuffing",
+"kale salad",
+"cranberry",
+"pumpkin pie",
+"ice cream",
+"corn"
+];
---
+++ meal.js
@@ -0,0 +1,16 @@
+
+
+var food = require('./foodArray');
+var inquirer = require("inquirer");
+
+
+//Making a constructor for the meal, which will take in three different portions(foods) from the food array
+var Meal = function(portion1, portion2, portion3) {
+  this.portion1 = portion1;
+  this.portion2 = portion2;
+  this.portion3 = portion3;
+
+
+
+
+module.exports = Meal;
